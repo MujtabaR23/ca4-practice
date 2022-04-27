580bda7116f5181523f104c1efda80b27de0d7e9
MujtabaR23 <s00221009@mail.itsligo.ie>
2022-04-27 13:03:06 +0100

    Add project files.

---
+++ ca4 practice/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+
+namespace ca4_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            graduate[] graduates = new graduate[4];
+
+            string template = "{0,-20}{1,-10}{2,-10}{3,-25}{4,-20}";
+            Console.WriteLine(template, "Graduate Number", "Name", "Marks", "Classification", "Project");
+
+            graduates[0] = new graduate("Mujtaba", 87, "Internet Of Things");
+            graduates[1] = new graduate("John", 50, "Programming");
+            graduates[2] = new graduate("Emma", 80, "Maths");
+            graduates[3] = new graduate("Sarah", 45, "Web Design");
+
+            for (int i = 0; i < graduates.Length; i++)
+            {
+                Console.WriteLine(graduates[i].ToString());
+            }
+
+            fileGrades();
+        }
+        static void fileGrades()
+        {
+            string lineIn;
+            graduate[] graduates = new graduate[10];
+            string[] fields = new string[3];
+            string template = "{0,-20}{1,-10}{2,-10}{3,-25}{4,-20}";
+
+            try
+            {
+                FileStream fs = new FileStream("text.csv", FileMode.Open, FileAccess.Read);
+
+                StreamReader inputStream = new StreamReader(fs);
+
+                Console.WriteLine("");
+                Console.WriteLine("Graduate Report");
+                Console.WriteLine();
+                Console.WriteLine(template, "Graduate Number", "Name", "Marks", "Classification", "Project");
+                Console.WriteLine();
+
+                lineIn = inputStream.ReadLine();
+
+                while(lineIn != null)
+                {
+                    for (int i = 0; i < graduates.Length; i++)
+                    {
+                        fields = lineIn.Split(',');
+                        IsInteger(fields[1], "Grade");
+                        graduates[i] = new graduate(fields[0], Int32.Parse(fields[1]), fields[2]);
+                        Console.WriteLine(graduates[i]);
+                        lineIn = inputStream.ReadLine();
+                    }
+                }
+                inputStream.Close();
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("File is invalid length");
+            }
+        }
+        static bool IsInteger(string textIn, string itemName)
+        {
+            bool isOk;
+            int num;
+
+            isOk = int.TryParse(textIn, out num);
+
+            if (isOk == true)
+                return true;
+            else
+            {
+                Console.WriteLine("{0} must be of type integer", itemName);
+                return false;
+            }
+
+        }
+    }
+}
---
+++ ca4 practice/graduate.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ca4_practice
+{
+    class graduate
+    {
+        private string _graduateName; //change to private
+        private int _average;
+        private int _graduateNumber;
+        public string _finalProject;
+
+        private static int _lastNumber;
+
+        public string Name
+        {
+            get
+            {
+                return _graduateName;
+            }
+            set
+            {
+                _graduateName = value;
+            }
+        }
+        public int Average
+        {
+            get
+            {
+                return _average;
+            }
+            set
+            {
+                _average = value;
+            }
+        }
+        public string FinalProject
+        {
+            get
+            {
+                return _finalProject;
+            }
+            set
+            {
+                _finalProject = value;
+            }
+        }
+        public int GraduateNumber
+        {
+            get
+            {
+                return this._graduateNumber;
+            }
+            set
+            {
+                this._graduateNumber = value;
+            }
+        }
+
+        public graduate()
+        {
+            _lastNumber++;
+            _graduateNumber = _lastNumber;
+        }
+
+        public graduate(string name, int average, string project)
+        {
+            _graduateName = name;
+            _average = average;
+            _finalProject = project;
+            _lastNumber++;
+            _graduateNumber = _lastNumber;
+        }
+
+        public string GetDegreeClassification(int mark)
+        {
+            string classification = "";
+
+            if (mark >= 70)
+            {
+                classification = "Distinction";
+            }
+            else if (mark >= 60 && mark <= 69)
+            {
+                classification = "Merit";
+            }
+            else if (mark >= 40 && mark <= 59)
+            {
+                classification = "Pass";
+            }
+            else if (mark < 40)
+            {
+                classification = "FAIL";
+            }
+
+            return classification;
+        }
+
+        public override string ToString()
+        {
+            return ($"{GraduateNumber,-20}{Name,-10}{Average,-10}{GetDegreeClassification(Average),-25}{FinalProject,-20}");
+        }
+    }
+}
